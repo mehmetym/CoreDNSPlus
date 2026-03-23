4bda61c89f627ebe129964fe4d00265859991a7d
mehmetym <97794083+mehmetym@users.noreply.github.com>
2026-03-24 00:53:26 +0300

    Eksik InputStream importu eklendi, derleme hatası giderildi.

--- app/src/main/java/dnsfilter/android/DNSProxyActivity.java
+++ app/src/main/java/dnsfilter/android/DNSProxyActivity.java
@@ -73,6 +73,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
