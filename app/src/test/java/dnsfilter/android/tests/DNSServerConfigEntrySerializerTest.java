3b5269eb6a30226d36951c03505a2e1de25ca422
MEHMET YILDIRIM <97794083+mehmetym@users.noreply.github.com>
2026-05-21 18:40:48 +0300

    Update DNSServerConfigEntrySerializerTest.java

--- app/src/test/java/dnsfilter/android/tests/DNSServerConfigEntrySerializerTest.java
+++ app/src/test/java/dnsfilter/android/tests/DNSServerConfigEntrySerializerTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-
 import dnsfilter.android.dnsserverconfig.widget.DNSServerConfigEntrySerializer;
 import dnsfilter.android.dnsserverconfig.widget.DNSType;
 import dnsfilter.android.dnsserverconfig.widget.listitem.DNSServerConfigEntry;
